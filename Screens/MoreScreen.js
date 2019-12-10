6728bf066acff2c4e256997c20fe6a03046da76f
BuiManhHieu <hieuori@gmail.com>
2019-12-10 15:42:54 +0700

    fix

--- Screens/MoreScreen.js
+++ Screens/MoreScreen.js
@@ -10,11 +10,10 @@ import {
   StyleSheet,
   ScrollView,
   Image,
-  Animated,
-  FlashList,
   Dimensions,
   Alert,
   FlatList,
+  Animated,
   TouchableOpacity,
   ActivityIndicator,
 } from 'react-native';
@@ -22,10 +21,12 @@ import Category from '../components/Category';
 import {articles_url, api_key} from '../config/rest_config';
 import {getData} from '../data/MoreScreenData';
 import RecommendItem from '../components/RecommendItem';
+const AnimatedFlatList = Animated.createAnimatedComponent(FlatList);
 class MoreScreen extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      animatedValue: new Animated.Value(0),
       isLoading: true,
       data: null,
     };
@@ -41,9 +42,14 @@ class MoreScreen extends Component {
   }
   render() {
     const {isLoading} = this.state;
+    let translateY = this.state.animatedValue.interpolate({
+			inputRange: [0, 265],
+			outputRange: [0, -265],
+			extrapolate: 'clamp',
+		});
     return (
-      <View style={{flex: 1, padding: 10}}>
-        <View>
+      <View style={{flex:1, padding:10}}>
+        <Animated.View style={[styles.headerWrapper, {transform: [{translateY}]}]}>
           <Category
             title="Bat Dong San"
             image="https://s3-alpha-sig.figma.com/img/c3f7/4be1/ecd76438ecf56e1d53c97b371c09546f?Expires=1576454400&Signature=WnrbGycxu2TCvkZ-~mZ0KtTJ6OeKW2A2HfnO9y1R618~dTQSfSFY9igabBW5EgJuLNzwlscaxEgDr1s1Pn9tv3IuZK2vAwLsKQtkNAlw~GuB49WbzL17faNfjYLzHdoiC5Nru24WqSPTn2vS9p0-SfNVNOpBLLFCCJusWaJHK1rUobCZvm6oKfnmf9GXNGgj2yLWL9KRmTl-ay~58Tu4ASDt0luXRQ2BLEysdGte-wJH42pFg44eYjJpGORGztB4So953HgxxGbrFRHMV7Trmpqiqv3k6PK3GjNXJXU9cnuqcBr3kuqnZmrr2LBLeMxQUwVizSAlPLemdDuZcEm5hw__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA"
@@ -52,20 +58,24 @@ class MoreScreen extends Component {
             title="Do Dien Tu"
             image="https://s3-alpha-sig.figma.com/img/62a9/fb5c/01f6ab2445263622f54b2ea81bf1ce52?Expires=1576454400&Signature=J2M9XGCTVxBY9o9MWkd-L14LDBjmDneoQ6NDUcnfG3~wSkCy4Ao5B5X0Hrxq161nVHkiQDfH3aAKfMWdYloLACLMHsqKvPulGN-Le5qoaF~fC8fNymHJXurBQOFwMRVTELj9ggxN4M846Qc7q4O21i2HOC8yAx6aLPrzWGWesoaBBwy6xg-~ooUrQqkGJ~5hJju1qntshjoH~YKKYaatrWVfgiBHt0gDqQumJJGVWyKefF3ULqiyfEE2QiIBS1vaqmQC54muVTQnJ27W-GSoY1lMMwgs7ShxTHZdrem2AlbcSisEW7aNa08StTPk0pnnuUBAqloiDo6uSw4T~jAIwQ__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA"
           />
-        </View>
-
-        <Text
+          <Text
           style={{
+            marginTop:10,
             color: 'black',
             fontWeight: 'bold',
             fontSize: 16,
-            paddingVertical: 10,
           }}>
           Có thể bạn quan tâm
         </Text>
-        <ActivityIndicator size="large" color ="red" animating= {isLoading}/>
-        <View>
-          <FlatList
+        </Animated.View>
+        <ActivityIndicator style={{marginTop:20}} size="large" color ="red" animating= {isLoading}/>
+          <AnimatedFlatList
+          contentContainerStyle={{marginTop: 260}}
+					scrollEventThrottle={16}
+					onScroll={Animated.event(
+						[{nativeEvent: {contentOffset: {y: this.state.animatedValue}}}],
+						{useNativeDriver: true}
+					)}
             data={this.state.data}
             renderItem={({item}) => (
               <TouchableOpacity
@@ -73,7 +83,7 @@ class MoreScreen extends Component {
                   this.props.navigation.navigate('ItemDetail', {item,data:this.state.data})
                 }>
                 <RecommendItem
-                image = {item.image}
+                  image = {item.image}
                   title={item.subject}
                   address={item.region}
                   price={item.price}
@@ -83,9 +93,16 @@ class MoreScreen extends Component {
             keyExtractor={item => item.list_id.toString()}
           />
         </View>
-      </View>
     );
   }
 }
-
+const styles = StyleSheet.create({
+	headerWrapper: {
+		position: 'absolute',
+		height: 250,
+		left: 0,
+		right: 0,
+    padding:10,
+	},
+});
 export default MoreScreen;
